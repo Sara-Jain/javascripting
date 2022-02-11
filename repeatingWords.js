547f56a63951c7fd510ebab7d467a4e30bc19c89
Sara-Jain <Sara_Jain@mckinsey.com>
2022-02-11 13:34:12 +0530

    fix: divide code into function

--- repeatingWords.js
+++ repeatingWords.js
@@ -1,17 +1,20 @@
-let s = 'This is is a sample string';
-let words = s.split(' ');
-let flag = false;
-const map1 = new Map();
-let j=0;
-for(let i=0 ; i<words.length ; i++){
-    if(map1.has(words[i])){
-        flag = true;
-    }else{
-        map1.set(words[i] , 1);
+const s = 'This is is a sample string';
+repeatingWords(s);
+
+function repeatingWords(s){
+    const words = s.split(' ');
+    let flag = false;
+    const wordMap = new Map();
+    for(let i=0 ; i<words.length ; i++){
+        if(wordMap.has(words[i])){
+            flag = true;
+        }else{
+            wordMap.set(words[i] , 1);
+        }
     }
-}
-if(flag){
-    console.log("String contains repeating words");
-}else{
-    console.log("Strings does not contain repeating words");
+    if(flag){
+        console.log("String contains repeating words");
+    }else{
+        console.log("Strings does not contain repeating words");
+    }
 }
